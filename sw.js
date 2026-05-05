32e5c36bd89ecaca051ba47b3f567302cc63b8e9
carlao <carlospedrosopoa@gmail.com>
2026-05-05 16:18:34 -0300

    UI: texto Aquecimento BT no menu

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'bt-online-v25';
+const CACHE_VERSION = 'bt-online-v26';
 const PRECACHE_URLS = ['./', './index.html', './style.css', './game.js', './manifest.json'];
 
 self.addEventListener('install', (event) => {
